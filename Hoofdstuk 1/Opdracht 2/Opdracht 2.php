115422b66bf2e3af60a089f8bdb1414b77b57b96
Menguelito <2112801@talnet.nl>
2021-04-14 13:29:26 +0200

    14 - 04 - 2021

---
+++ Hoofdstuk 1/Opdracht 2/Opdracht 2.php
@@ -0,0 +1,8 @@
+<?php
+$a = "die geen fouten";
+$b = "en";
+$c = "niet";
+
+$volledigeZin = "Mensen ". $a . " Maken, werken ". $c. ".";
+
+echo $volledigeZin;
---
+++ Hoofdstuk 1/Opdracht 3/Opdracht 3.php
@@ -0,0 +1,8 @@
+<?php
+$a = "ry";
+$b = ", but";
+$c = "fail";
+
+$volledigeZin = "T". $a. $b. " never ". $c. " to ". "t".$a;
+
+echo $volledigeZin;
