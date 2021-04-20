ac46ce7cc30c4f4f860c1e64a82f6df155b57903
Menguelito <2112801@talnet.nl>
2021-04-20 11:09:53 +0200

    20-04-2021

---
+++ Hoofdstuk 3/Opdracht 1/Opdracht 1.php
@@ -0,0 +1,9 @@
+<?php
+        echo "<style> body{text-align: center} </style>";
+        echo "<br>";
+        for ($Kerst = 0; $Kerst <= 9; $Kerst++) {
+            for($Boom = 0; $Boom <= $Kerst; $Boom++) {
+        echo "*";
+    }
+    echo "* <br>";
+}
